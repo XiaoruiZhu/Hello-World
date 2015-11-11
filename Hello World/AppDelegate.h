9feb0cc3bc058f35e74475892bb652767fa59600
朱筱妞妞 <zyy8_101@163.com>
2015-11-10 23:35:36 -0500

    Initial Commit

---
+++ Hello World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello World
+//
+//  Created by Xiaorui(Jeremy) Zhu on 11/10/15.
+//  Copyright © 2015 AmEveryone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hello World/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello World
+//
+//  Created by Xiaorui(Jeremy) Zhu on 11/10/15.
+//  Copyright © 2015 AmEveryone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
